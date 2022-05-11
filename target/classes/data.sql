0b4f9376e0d9bd1106961d0c06cc867df508813b
Joaocastro20 <98284593+Joaocastro20@users.noreply.github.com>
2022-05-11 16:00:23 -0300

    teste html com findall unversioned

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER (id,nm_name,email) VALUES (1,'eustaquio','eustaquio@Gmail.com');
+INSERT INTO USER (id,nm_name,email) VALUES (2,'robertinho','robertinho@Gmail.com');
+INSERT INTO USER (id,nm_name,email) VALUES (3,'ana','ana@Gmail.com');
+INSERT INTO USER (id,nm_name,email) VALUES (4,'ronaldo','ronaldo@Gmail.com');
+INSERT INTO USER (id,nm_name,email) VALUES (5,'diogo','diogo@Gmail.com');
