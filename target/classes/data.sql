62bd51209ca270f2e6a70b7afd4102b769f9461e
Joaocastro20 <98284593+Joaocastro20@users.noreply.github.com>
2022-05-18 18:37:29 -0300

    tamanho da linha de assinatura automatica

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,27 +1,26 @@
-INSERT INTO USER (id,cpf,nm_name,email) VALUES (1,'111.111.111-11','robertinho','robertinho@Gmail.com');
-INSERT INTO USER (id,cpf,nm_name,email) VALUES (2,'222.222.222-22','eustaquio','eustaquio@Gmail.com');
-INSERT INTO USER (id,cpf,nm_name,email) VALUES (3,'333.333.333-33','ana','ana@Gmail.com');
-INSERT INTO USER (id,cpf,nm_name,email) VALUES (4,'444.444.444-44','ronaldo','ronaldo@Gmail.com');
-INSERT INTO USER (id,cpf,nm_name,email) VALUES (5,'555.555.555-55','diogo','diogo@Gmail.com');
+INSERT INTO USER (id,cpf,nm_name,email) VALUES (1,'111.111.111-11','Robertinho','robertinho@Gmail.com');
+INSERT INTO USER (id,cpf,nm_name,email) VALUES (2,'222.222.222-22','Eustaquio','eustaquio@Gmail.com');
+INSERT INTO USER (id,cpf,nm_name,email) VALUES (3,'333.333.333-33','Ana','ana@Gmail.com');
+INSERT INTO USER (id,cpf,nm_name,email) VALUES (4,'444.444.444-44','Ronaldo','ronaldo@Gmail.com');
+INSERT INTO USER (id,cpf,nm_name,email) VALUES (5,'555.555.555-55','Diogo','diogo@Gmail.com');
 
 INSERT INTO TEMPLATE_DOCUMENTO (id,ativo,modelo,nome) VALUES  (1,true,'<html><head><link href="classpath:/template.css" rel="stylesheet"/></head><body><img src="classpath:/basis.png" /> <p class="classpath:/template.css"> Eu ${user.name}, identificado pelo CPF: ${user.cpf}</p></body></html>','termo');
 INSERT INTO TEMPLATE_DOCUMENTO (id,ativo,modelo,nome) VALUES  (2,true,'<html><head><link href="classpath:/template.css" rel="stylesheet"/></head><body><img src="classpath:/basis.png" /> <p class="classpath:/template.css"> <h1>TERMO DE COMPROMISSO DE MANUTENÇÃO DE SIGILO<br></br><hr></hr></h1>
 <p>
   A Basis Tecnologia da Informação S.A., sediada em ST SCS
   QUADRA 8 BLOCO B - Lotes, 50/60 - Venâncio shopping-SALAS 824 a 842
-  (pares) - Asa Sul, Brasília, DF, CNPJ n.° 11.777.162/0001-57,doravante
-  denominado CONTRATANTE, e, de outro lado, o(a) Kely Cristina Feitosa
-  dos Santos CPF: 604.829.613-41 e RG 038321752009-7 SSP/MA nos quais
+  (pares) - Asa Sul, Brasília, DF,doravante
+  denominado CONTRATANTE, e, de outro lado, o(a) ${user.name} CPF: ${user.cpf} SSP/MA nos quais
   o FUNCIONÁRIO poderá ter acesso a informações sigilosas dos clientes da
   CONTRATANTE; CONSIDERANDO a necessidade de ajustar as condições
   de revelação destas informações sigilosas, bem como definir as regras para o
   seu uso e proteção;
 </p></p>
-<div style="text-align: center">
+<div style="align-items: center">
 <table style="text-align: center">
   <tr>
-    <td>___________________________</td>
-    <td>___________________________</td>
+    <td><hr style="width:100%"></hr></td>
+    <td><hr style="width:100%"></hr></td>
   </tr>
   <tr>
     <td>Basis Tecnologia</td>
@@ -29,5 +28,7 @@ INSERT INTO TEMPLATE_DOCUMENTO (id,ativo,modelo,nome) VALUES  (2,true,'<html><he
   </tr>
 </table>
 </div>
-</body></html>','termo');
+</body></html>','tamanhoautomatico');
+INSERT INTO TEMPLATE_DOCUMENTO (id,ativo,modelo,nome) VALUES  (3,true,'<html><head><link href="classpath:/template.css" rel="stylesheet"/></head><body><img src="classpath:/basis.png" /> <p class="classpath:/template.css"> Eu ${user.name}, identificado pelo CPF: ${user.cpf}</p></body></html>','termo');
+
 
