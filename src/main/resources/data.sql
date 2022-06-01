2170c23f2bb715509ed3d0e79942998216e1f290
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-06-01 12:54:28 -0300

    Modificaçao nos templates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -236,8 +236,10 @@ INSERT INTO TEMPLATE_DOCUMENTO (id,ativo,modelo,nome) VALUES  (3,true,'<html><he
       <td>Assinatura:__________________</td>
     </tr>
   </table>
+  <div class="profile_picture" style="display:block;width:140px;height:140px;" />
 </div>
 </body></html>
+
 ','termo');
 
 
